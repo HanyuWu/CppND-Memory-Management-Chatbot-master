a8cde9f3e9e5897d7500ff136bf0010257665c6a
HanyuWu <wuh199410@gmail.com>
2020-04-04 18:00:22 -0400

    [Hanyu] #4 Finish Task 3

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,7 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    // delete _chatBot;     _chatBot is owned by graphnode.
+    delete _chatBot;  
 
     ////
     //// EOF STUDENT CODE
